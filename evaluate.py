fe40e76953969624936bcc507850f9bf3010b9b2
PraptiKate <praptikate025@gmail.com>
2026-04-23 21:08:22 +0530

    Create evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,18 @@
+import json
+import os
+import random
+
+username = os.getenv("GITHUB_ACTOR", "unknown")
+
+# TEMP random score (we fix later)
+accuracy = round(random.uniform(0.7, 0.95), 3)
+f1 = round(random.uniform(0.7, 0.95), 3)
+
+result = {
+    "name": username,
+    "accuracy": accuracy,
+    "f1": f1
+}
+
+with open("result.json", "w") as f:
+    json.dump(result, f)
